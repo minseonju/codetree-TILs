1df9ddbf32d0ac77e1192863decbe0fb324e6c52
minseonju <138373881+minseonju@users.noreply.github.com>
2024-05-11 22:09:58 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240511/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float a;
+    cin >> a;
+
+    float result=a+1.5;
+
+    cout<<fixed;
+    cout.precision(2);
+    cout<<result;
+
+    
+   
+    return 0;
+}
