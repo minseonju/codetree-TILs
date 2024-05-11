b3bca51cc231a806f3067a2cd91c5eefcc3269b6
minseonju <138373881+minseonju@users.noreply.github.com>
2024-05-11 22:56:52 +0900

    Title: 2개의 정수를 서로 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240511/2개의 정수를 서로 더하기/add-two-integers-each-other.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    cin >>a>>b;
+
+    int result_1=a+b;
+    int result_2=result_1+b;
+
+    cout<<result_1<<" "<<result_2;
+    return 0;
+}
