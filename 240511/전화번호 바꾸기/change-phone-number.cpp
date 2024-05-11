2743daedf3eb820458f3bd12915abac17b32da1c
minseonju <138373881+minseonju@users.noreply.github.com>
2024-05-11 22:46:33 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240511/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> c;
+    cin.get();
+    cin >> a;
+    cin.get();
+    cin >> b;
+
+    cout << 0 << c << "-" << b << "-" << a;
+    return 0;
+}
