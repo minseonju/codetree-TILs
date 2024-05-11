6c40fbdc0bd63257f4eb39bb23ed5cd543667fdf
minseonju <138373881+minseonju@users.noreply.github.com>
2024-05-11 23:54:16 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240511/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+
+    cin >>a>>b;
+
+    float sum=a+b;
+    float average= sum/2;
+
+    cout <<sum<<" "<< average;
+
+    return 0;
+}
