8220fa8c9f2620c8cc3c30519c245ed9700b9544
minseonju <138373881+minseonju@users.noreply.github.com>
2024-05-11 22:33:34 +0900

    Title: 날짜 변경하여 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240511/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int mm;
+    int dd;
+    int yyyy;
+
+    cin>>yyyy;
+    cin.get();
+    cin>>mm;
+    cin.get();
+    cin>>dd;
+
+    cout<<mm<<"-"<<dd<<"-"<<yyyy;
+    return 0;
+}
